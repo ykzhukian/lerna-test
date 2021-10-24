1a4eae57c7f89caab042538a9e6705cab8b6d6b4
祝云开 <kianzyk@gmail.com>
2021-10-24 21:10:41 +0800

    revert publish

--- core/core/bin/index.js
+++ core/core/bin/index.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 
-const importLocal = require('import-local')
+const utils = require('@lerna-test/utils')
 
-if (importLocal(__filename)) {
-  require('npmlog').info('cli', '正在使用本地版本')
-} else {
-  require('../lib')(process.argv.slice(2))
-}
-
-const utils = require('@lerna-test-cool/utils')
 console.log('greeting')
 utils()
