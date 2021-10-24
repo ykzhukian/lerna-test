75ebb23e3fe07c3f7c41bae09f4edd377cac6b0d
祝云开 <kianzyk@gmail.com>
2021-10-24 21:12:47 +0800

    fix

--- core/core/bin/index.js
+++ core/core/bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const utils = require('@lerna-test/utils')
+const utils = require('@lerna-test-cool/utils')
+const importLocal = requre('import-local')
 
 console.log('greeting')
 utils()
